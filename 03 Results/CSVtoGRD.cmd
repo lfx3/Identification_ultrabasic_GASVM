f207789e4e4138ea7e8f9e10b9056d26bc032b63
lfx3 <103985124+lfx3@users.noreply.github.com>
2024-07-25 00:01:33 +0800

    Add files via upload

---
+++ 03 Results/CSVtoGRD.cmd
@@ -0,0 +1,9 @@
+*****************************************************************  
+"Inputting CSV file needing transform: ",Magnetic_radioactivity_GASVM_result.csv
+*******************GRD paremeters****************************
+"Points number of X and Y directions: ",1313,765
+"Minimum and maximum Y coordinates: ",341000,669000
+"Minimum and maximum Y coordinates: ",4208000,4399000
+******************************************************************
+"Outputing GRD file name: ",Magnetic_radioactivity_GASVM_result.grd
+******************************************************************
